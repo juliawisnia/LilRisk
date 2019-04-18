624ac4d575a268be4068bcffeb7c581d3d81e98a
Julia Wisnia <wisnia@usc.edu>
2019-04-18 15:55:52 -0700

    sql

--- CSCI201_FinalProject/src/main/java/Servlets/PurchaseServlet.java
+++ CSCI201_FinalProject/src/main/java/Servlets/PurchaseServlet.java
@@ -12,8 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 public class PurchaseServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
 	}
-
-
 }
