cb6d7610a05a01461795a1e03b06a33879e36e72
Alan Chang <alangcha@usc.edu>
2019-04-18 15:49:40 -0700

    Update CSCI201FinalProject.sql

--- CSCI201FinalProject.sql
+++ CSCI201FinalProject.sql
@@ -39,22 +39,31 @@ INSERT INTO User (username, userPassword)
 	VALUES ('TestUsername1', 'TestPassword1'),
 		   ('TestUsername2', 'TestPassword2'),
 		   ('TestUsername3', 'TestPassword3'),
-           ('Julia','Wisnia');
+                   ('Julia','Wisnia');
            
 INSERT INTO Portfolio (userID, portfolioName)
 	VALUES (1, 'TestPortfolio'),
-		   (4, 'First');
+		   (4, 'Julia'),
+          	   (4, 'Mallika'),
+       	           (4, 'Robert'),
+                   (4, 'Alan');
 
 INSERT INTO Positions (portfolioID, symbol, buyTime, buyPrice, amount)
-	VALUES (1, 'BTCUSDT', 'April 10 9:16:05', '5014.56', '0.358420'),
-		   (1, 'ETHUSDT', 'April 11 14:34:25', '163.75', '1.5673'),
-		   (1, 'XRPUSDT', 'April 11 20:54:15', '0.32236', '457.2312');
+	VALUES (1, 'BTCUSDT', '1555627432', '5014.56', '0.358420'),
+		   (1, 'ETHUSDT', '1555627432', '163.75', '1.5673'),
+		   (1, 'XRPUSDT', '15556274325', '0.32236', '457.2312'),
+          	   (2, 'BTCUSDT', '1555627432', '5214.56', '1.358420'),
+       	           (2, 'ETHUSDT', '1555627432', '173.75', '12.5673'),
+		   (3, 'ETHUSDT', '1555627432', '173.75', '12.5673'),
+		   (4, 'XRPUSDT', '15556274325', '0.32236', '457.2312'),
+      	           (5, 'BTCUSDT', '1555627432', '4214.56', '1.358420');
+           
 
 INSERT INTO Trades (portfolioID, symbol, buyTime, sellTime, buyPrice, sellPrice, amount)
-	VALUES (1, 'BTCUSDT', 'April 10 9:16:05', 'April 11 9:16:05', '5014.56', '5236.84', '0.358420'),
-		   (1, 'ETHUSDT', 'April 11 14:34:25', 'April 12 9:16:05', '163.75', '154.35', '1.5673'),
-		   (1, 'XRPUSDT', 'April 11 20:54:15', 'April 12 9:16:05', '0.32236', '0.37865', '457.2312'),
-           (4, 'BTCUSDT', 'April 10 9:16:05', 'April 11 9:16:05', '5214.56', '5436.84', '1.358420'),
-		   (4, 'ETHUSDT', 'April 11 14:34:25', 'April 12 9:16:05', '173.75', '181.35', '12.5673'),
-		   (4, 'XRPUSDT', 'April 11 20:54:15', 'April 12 9:16:05', '0.32236', '0.37865', '457.2312');
+	VALUES (1, 'BTCUSDT', '1555627432', 'April 11 9:16:05', '5014.56', '5236.84', '0.358420'),
+		   (1, 'ETHUSDT', '1555627432', 'April 12 9:16:05', '163.75', '154.35', '1.5673'),
+		   (1, 'XRPUSDT', '1555627432', 'April 12 9:16:05', '0.32236', '0.37865', '457.2312'),
+           	   (4, 'BTCUSDT', '1555627432', 'April 11 9:16:05', '5214.56', '5436.84', '1.358420'),
+		   (4, 'ETHUSDT', '1555627432', 'April 12 9:16:05', '173.75', '181.35', '12.5673'),
+		   (4, 'XRPUSDT', '1555627432', 'April 12 9:16:05', '0.32236', '0.37865', '457.2312');
     
