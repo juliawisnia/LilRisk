efedf8219a43f310fed837a58d581fed33b3d2f4
Robert Steele <rjsteele@usc.edu>
2019-04-19 19:20:48 -0700

    s

--- CSCI201_FinalProject/src/main/java/coinData/PortfolioClass.java
+++ CSCI201_FinalProject/src/main/java/coinData/PortfolioClass.java
@@ -401,7 +401,7 @@ public class PortfolioClass {
 	public String getPieData() {
 		String ret = "";
 		for (Map.Entry<String,Position> entry : coins.entrySet()) {
-			System.out.println(entry.getKey());
+			System.out.println("Getting pie data for: " + entry.getKey());
 			ret += ",['" + entry.getKey() + "'," + entry.getValue().getTotalValue() + "]";
 		}
 		return ret;
